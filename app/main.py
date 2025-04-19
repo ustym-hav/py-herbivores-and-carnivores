933b038d50ef5b8c3cd6a5e0352fa99becd6e329
Ustym <Ustym.hav@gmail.com>
2025-04-19 20:18:39 +0300

    animal_kingdom

--- app/main.py
+++ app/main.py
@@ -1,1 +1,32 @@
-# write your code here
+from typing import List
+
+
+class Animal:
+    alive: List["Animal"] = []
+
+    def __init__(
+            self, name: str, health: int = 100, hidden: bool = False,
+    ) -> None:
+        self.name = name
+        self.health = health
+        self.hidden = hidden
+        Animal.alive.append(self)
+
+    def __repr__(self) -> str:
+        return (f"{{Name: {self.name}, Health: {self.health}, "
+                f"Hidden: {self.hidden}}}")
+
+
+class Herbivore(Animal):
+
+    def hide(self) -> None:
+        self.hidden = not self.hidden
+
+
+class Carnivore(Animal):
+
+    def bite(self, prey: Herbivore) -> None:
+        if isinstance(prey, Herbivore) and not prey.hidden:
+            prey.health = prey.health - 50
+        if prey.health <= 0 and prey in Animal.alive:
+            Animal.alive.remove(prey)
